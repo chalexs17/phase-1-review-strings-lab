c1e7260612bd03a23bcdcb1d49c559ea340f3622
Alexis Chavez <alexis.chavez17@outlook.com>
2023-03-16 14:47:02 -0700

    Complete JS Review Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Alexis';
+//Write our welcome messaged using backticks and template literal instead of "+".
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//Now write with excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//Now write with a shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
